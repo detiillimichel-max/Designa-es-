d23d87328feb1e0ea0fca388e3f2dd2796eeb8fb
Michel Detiilli <detiillimichel@gmail.com>
2026-09-13 11:00:54 -0300

    Create membros.js with dirigente and elegiveis data
    Added constants for DIRIGENTE_FIXO and ELEGIVEIS arrays.

---
+++ src/data/membros.js
@@ -0,0 +1,24 @@
+export const DIRIGENTE_FIXO = "Cecílio";
+
+export const ELEGIVEIS_DISCURSO = [
+  "Cristiano",
+  "Rodrigo Soares",
+  "Wellington",
+  "Rodrigo Ramos",
+  "Cecílio",
+  "Ribamar",
+  "Cleyton",
+  "Felipe Ramos"
+];
+
+export const ELEGIVEIS_LEITOR = [
+  "Cristiano",
+  "Rodrigo Soares",
+  "Wellington",
+  "Rodrigo Ramos",
+  "Ribamar",
+  "Cleyton",
+  "Felipe Ramos",
+  "Davi",
+  "Ivanilson"
+];
